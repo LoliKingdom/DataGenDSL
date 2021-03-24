545592f052c972706093166c861e965a50f5bb55
KYLE <minecraft.kyle.train@gmail.com>
2021-03-24 16:09:23 +0800

    add more conditions.

--- src/main/kotlin/io/github/chaos/dgdsl/builder/loot/LootConditionBuilder.kt
+++ src/main/kotlin/io/github/chaos/dgdsl/builder/loot/LootConditionBuilder.kt
@@ -3,7 +3,12 @@ package io.github.chaos.dgdsl.builder.loot
 import io.github.chaos.dgdsl.builder.loot.conditions.ILootConditionCollector
 import io.github.chaos.dgdsl.builder.utils.IConcentrator2List
 import io.github.chaos.dgdsl.builder.utils.IListInfixFunctions
+import net.minecraft.advancements.criterion.DamageSourcePredicate
+import net.minecraft.advancements.criterion.StatePropertiesPredicate
+import net.minecraft.block.Block
 import net.minecraft.loot.conditions.Alternative
+import net.minecraft.loot.conditions.BlockStateProperty
+import net.minecraft.loot.conditions.DamageSourceProperties
 import net.minecraft.loot.conditions.ILootCondition
 
 class LootConditionBuilder : IListInfixFunctions {
@@ -26,4 +31,25 @@ class LootConditionBuilder : IListInfixFunctions {
         override fun collect(): ILootCondition.IBuilder =
             builder.apply { conditions.forEach(::alternative) }
     }
+
+    /**
+     *  BlockState Property
+     */
+
+    fun blockStateProperty(block: Block) =
+        conditions add BlockStateProperty.builder(block)
+
+    fun blockStateProperty(block: Block, property: StatePropertiesPredicate.Builder) =
+        conditions add BlockStateProperty.builder(block).fromProperties(property)
+
+    /**
+     *  Damage Source Properties
+     */
+
+    fun damageSourceProperty(builder: DamageSourcePredicate.Builder) =
+        conditions add DamageSourceProperties.builder(builder)
+
+    /**
+     *
+     */
 }
